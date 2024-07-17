779b825dce1f80ac836e9a3249de862fa98b40da
Aayush-1205 <shindeaayush250@gmail.com>
2024-07-17 14:00:59 +0530

    MemberShip - structure

---
+++ src/pages/MemberShip.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MemberShip = () => {
+    return (
+        <div className='p-4 w-full h-full flex flex-col items-center'>
+            {/* Mahak */}
+            <div></div>
+
+            {/* Aayush */}
+            <div></div>
+
+            {/* Gaurav */}
+            <div></div>
+        </div>
+    )
+}
+
+export default MemberShip
