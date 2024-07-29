920bb30f67c31190551420e3d26e5762dee7a6d7
Aayush-1205 <shindeaayush250@gmail.com>
2024-07-29 18:37:08 +0530

    New Changes - landing page

--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -29,9 +29,9 @@ const Subscription = () => {
                         required
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className='border border-black w-[80%] mt-14 p-4 max-md:mt-12'
+                        className='border border-black w-[80%] mt-14 px-4 py-2 text-sm lg:p-4 max-md:mt-12'
                     />
-                    <button type='submit' className='w-[80%] bg-rose-700 text-white text-lg rounded-lg hover:bg-rose-600 font-[600] py-4 my-4'>
+                    <button type='submit' className='w-[80%] bg-rose-700 text-white text-sm lg:text-lg rounded-lg hover:bg-rose-600 font-[600] py-2 lg:py-4 my-4'>
                         Subscribe
                     </button>
                 </fieldset>
