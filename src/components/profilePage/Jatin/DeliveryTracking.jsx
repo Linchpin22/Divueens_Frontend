9db5c8ff9d4659c28f9fbe03db2300b8325e3b85
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-08-03 14:29:35 +0530

    commeting changes on DeliveryTracking myOrder and ProductListItemData and WishList

--- src/components/profilePage/Jatin/DeliveryTracking.jsx
+++ src/components/profilePage/Jatin/DeliveryTracking.jsx
@@ -1,123 +1,44 @@
-import React, { useState } from "react";
-import OrderTrackingStepper from "./OrderTrackingStepper";
+import React from "react";
 
-const DeliveryTracking = () => {
-  const [orderDetails, setOrderDetails] = useState({
-    customerName: "XYZ ABC",
-    contactNo: "+91 99999XXXXX",
-    deliveryAddress:
-      "306, North Plaza Near 4D square mall, Sabarmati, Ahemdabad, 38005",
-    orderID: "DSQ3Y8798980",
-    orderStatus:1,
-  });
-  const trackingSteps=[
-    {
-        stepName:'Order Recived',
-        estimatedDnT:{
-            date:'26 July 2024',
-            time:'3:00PM'
-        }
-    },
-    {
-        stepName:'Order Picked Up',
-        estimatedDnT:{
-            date:'26 July 2024',
-            time:'3:00PM'
-        }
-    },
-    {
-        stepName:'Out For Delivery ',
-        estimatedDnT:{
-            date:'26 July 2024',
-            time:'3:00PM'
-        }
-    },
-    {
-        stepName:'Delivered',
-        estimatedDnT:{
-            date:'26 July 2024',
-            time:'3:00PM'
-        }
-    }
-  ]
+const DeliveryTracking = ({ orderStatus }) => {
+  const trackingSteps = {
+    Delivered: [
+      { stepName: 'Order Received', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+      { stepName: 'Order Picked Up', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+      { stepName: 'Out For Delivery', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+      { stepName: 'Delivered', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+    ],
+    "In Progress": [
+      { stepName: 'Order Received', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+      { stepName: 'Order Picked Up', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+      { stepName: 'Out For Delivery', estimatedDnT: { date: '26 July 2024', time: '3:00PM' } },
+    ],
+    Cancelled: [
+      { stepName: 'Order Cancelled', estimatedDnT: { date: 'N/A', time: 'N/A' } },
+    ],
+  };
 
+  const steps = trackingSteps[orderStatus] || [];
 
   return (
-    <div className="grid grid-cols-1 justify-center w-full lg:grid-cols-3 mt-16">
-      <div className="w-full col-span-1">
-        <div className="border-b-2 py-4 text-center">
-          <h1 className="md:text-2xl text-lg font-bold text-nowrap ">
-            Order Details
-          </h1>
-        </div>
-        <div className="font-semibold flex flex-col mt-20 mx-auto w-fit max-w-96 text-center my-8">
-          <div>
-            <p className="text-black md:text-lg text-base">Customer Name</p>
-            <p className="text-gray-500 mb-8 pb-4  md:text-base mt-2 text-sm">
-              {orderDetails.customerName}
-            </p>
+    <div className="mt-4">
+      <div className="relative pl-8">
+        <div className="absolute top-0 left-3 h-full border-l-2 border-gray-300"></div>
+        {steps.map((step, index) => (
+          <div key={index} className="flex items-center mb-4">
+            <div
+              className={`w-6 h-6 rounded-full border-4 border-white ${
+                index === steps.length - 1 ? 'bg-pink-600' : 'bg-gray-300'
+              }`}
+            ></div>
+            <div className="ml-4">
+              <p className="font-bold">{step.stepName}</p>
+              <p className="text-gray-500">
+                {step.estimatedDnT.date} at {step.estimatedDnT.time}
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="text-black md:text-lg text-base">Contact Number</p>
-            <p className="text-gray-500 mb-8 pb-4  md:text-base mt-2 text-sm">{orderDetails.contactNo}</p>
-          </div>
-          <div>
-            <p className="text-black md:text-lg text-base">Delivery Address</p>
-            <p className="text-gray-500 mb-8 pb-4  md:text-base mt-2 text-sm">
-              {orderDetails.deliveryAddress}
-            </p>
-            <br />
-          </div>
-        </div>
-      </div>
-      <div className="w-full col-span-2">
-        <div className="border-b-2 py-4 text-center">
-          <h1 className="md:text-2xl text-lg font-bold text-nowrap">
-            Order ID: #{orderDetails.orderID}
-          </h1>
-        </div>
-        <div className="font-semibold mx-auto w-fit text-center my-8">
-          <h2 className="md:text-xl text-base font-bold mb-4">Track Your Order</h2>
-          {/* <div className="relative pl-8">
-            <div className="absolute top-0 left-3 h-full border-l-2 border-gray-300">
-            </div>
-            <div className="flex items-center mb-8">
-              <div className="w-6 h-6 bg-gray-300 rounded-full border-4 border-white"></div>
-              <div className="ml-4">
-                <p className="font-bold">Order Delivered</p>
-                <p className="text-gray-500">
-                  Estimated by 10 July 2024 at 3:00 PM
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center mb-8">
-              <div className="w-6 h-6 bg-gray-300 rounded-full border-4 border-white"></div>
-              <div className="ml-4">
-                <p className="font-bold">Out For Delivery</p>
-                <p className="text-gray-500">
-                  Estimated by 10 July 2024 at 3:00 PM
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center mb-8">
-              <div className="w-6 h-6 bg-gray-300 rounded-full border-4 border-white"></div>
-              <div className="ml-4">
-                <p className="font-bold">Order Picked Up</p>
-                <p className="text-gray-500">
-                  Estimated by 10 July 2024 at 3:00 PM
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center mb-8">
-              <div className="w-6 h-6 bg-pink-600 rounded-full border-4 border-white"></div>
-              <div className="ml-4">
-                <p className="font-bold">Order Received</p>
-                <p className="text-gray-500">10 July 2024 at 3:00 PM</p>
-              </div>
-            </div>
-          </div> */}
-          <OrderTrackingStepper stepConfig={trackingSteps}/>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/profilePage/Jatin/myOrder2.jsx
+++ src/components/profilePage/Jatin/myOrder2.jsx
@@ -1,156 +1,137 @@
-import React, { useState } from "react";
-import orderData from "./orderData2";
-import rightarrow from "../../../assets/myOrders/right-arrow.png";
-import Delete from "../../../assets/myOrders/bin.png";
-import DeliveryTracking from "./DeliveryTracking";
-import { MdKeyboardArrowDown, MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-export default function MyOrder2() {
-  const [activeButton, setActiveButton] = useState("All");
-  const [filteredOrders, setFilteredOrders] = useState(orderData);
-  const [showTracking, setShowTracking] = useState(false);
+// import React, { useState } from "react";
+// import orderData from "./orderData";
+// import { MdKeyboardArrowDown, MdKeyboardArrowRight } from "react-icons/md";
+// import DeliveryTracking from "./DeliveryTracking";
 
-  const handleButtonClick = (value) => {
-    setActiveButton(value);
-    if (value === "All") {
-      setFilteredOrders(orderData);
-    } else {
-      const filteredData = orderData.filter((order) => order.status === value);
-      setFilteredOrders(filteredData);
-    }
-  };
+// export default function MyOrder2() {
+//   const [activeButton, setActiveButton] = useState("All");
+//   const [filteredOrders, setFilteredOrders] = useState(orderData);
+//   const [showTracking, setShowTracking] = useState(null);
 
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <nav className="mb-6">
-        <ol className="flex flex-wrap space-x-2  text-black font-bold">
-          <li>Home</li>
-          <li>&gt;</li>
-          <li>My Account</li>
-          <li>&gt;</li>
-          <li>My Orders</li>
-        </ol>
-      </nav>
-      <div className="flex flex-col gap-2 md:flex-row justify-between items-start my-4">
-        <div className="flex flex-wrap w-full md:w-fit flex-col gap-2 md:flex-row">
-          <button 
-            onClick={() => handleButtonClick("All")}
-            className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${
-              activeButton === "All" ? "bg-rose-200" : "hover:bg-rose-300"
-            }`}
-          >
-            All
-          </button>
-          <button 
-            onClick={() => handleButtonClick("In Progress")}
-            className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${
-              activeButton === "In Progress"
-                ? "bg-rose-200"
-                : "hover:bg-rose-300"
-            }`}
-          >
-            In Progress
-          </button>
-          <button 
-            onClick={() => handleButtonClick("Delivered")}
-            className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${
-              activeButton === "Delivered" ? "bg-rose-200" : "hover:bg-rose-300"
-            }`}
-          >
-            Delivered
-          </button>
-          <button 
-            onClick={() => handleButtonClick("Cancelled")}
-            className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${
-              activeButton === "Cancelled" ? "bg-rose-200" : "hover:bg-rose-300"
-            }`}
-          >
-            Cancelled
-          </button>
-        </div>
-        <div className="flex w-full md:w-fit items-center">
-          <button className="py-2.5 px-5 w-full md:w-fit  text-sm font-medium text-white bg-rose-400 rounded-full border border-black hover:bg-rose-300 flex items-center">
-            <span className="mx-auto">Track Order</span>
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between ">
-        {filteredOrders.map((order, index) => (
-          <div key={index} className="bg-white shadow-md w-full rounded-md md:p-12 p-8">
-            <div className="flex justify-end w-full">
-              <img
-                className=" top-2 right-4 w-[20px] h-[20px] cursor-pointer"
-                src={Delete}
-                alt="Bin Icon"
-              />
-            </div>
-            <div className="flex flex-col gap-8 md:flex-row px-12 items-center">
-                <img
-                  src={order.imageUrl}
-                  alt="Product Image"
-                  className="md:w-28 w-40 object-cover rounded-md mb-4 sm:mb-0 sm:mr-4"
-                />
-              <div className="flex items-start justify-between flex-col md:flex-row mr-12 w-full gap-8 md:items-center">
-                <div className="">
-                  <div className="text-gray-500 mb-2">
-                    Order ID: {order.orderId}
-                  </div>
-                  <div className="text-black font-semibold text-xs mb-1">
-                    {order.description}
-                  </div>
-                  <div
-                    className={`text-${
-                      order.rating >= 4 ? "yellow" : "gray"
-                    }-500 flex items-center`}
-                  >
-                    <div className="text-black text-xs font-semibold mr-1">
-                      Rating:
-                    </div>
-                    {"★".repeat(order.rating)}
-                    {"☆".repeat(5 - order.rating)}
-                  </div>
-                  <div className="font-semibold">{order.price}</div>
-                </div>
-                <div className="">
-                  <div className="font-medium text-[10px] sm:text-xs lg:text-base text-gray-400">
-                    Status
-                  </div>
-                  <div className="font-bold text-[10px] sm:text-sm lg:text-lg">
-                    {order.status}
-                  </div>
-                </div>
-                <div className=" ">
-                  <div className="font-medium text-[8px] sm:text-xs lg:text-sm text-gray-400">
-                    {order.datetitle}
-                  </div>
-                  <div className="font-bold text-[10px] sm:text-sm lg:text-lg">
-                    {order.date}
-                  </div>
-                </div>
-              </div>
-              {/* <img
-                className=" right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 cursor-pointer"
-                src={rightarrow}
-                alt="Right Arrow Icon" 
-                
-              /> */}
-              {showTracking?<MdKeyboardArrowDown
-                className="text-5xl cursor-pointer"
-                onClick={() => {
-                  setShowTracking((old) => !old);
-                }}
-              />:<MdKeyboardArrowRight
-              className="text-5xl cursor-pointer"
-              onClick={() => {
-                setShowTracking((old) => !old);
-              }}
-            />}
-            </div>
-            {/* <div className="flex "> */}
-            {showTracking && <DeliveryTracking />}
-            {/* </div> */}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+//   const handleButtonClick = (value) => {
+//     setActiveButton(value);
+//     if (value === "All") {
+//       setFilteredOrders(orderData);
+//     } else {
+//       const filteredData = orderData.filter((order) => order.status === value);
+//       setFilteredOrders(filteredData);
+//     }
+//   };
+
+//   const toggleTracking = (index) => {
+//     setShowTracking(showTracking === index ? null : index);
+//   };
+
+//   return (
+//     <div className="min-h-screen bg-gray-100 p-4">
+//       <nav className="mb-6">
+//         <ol className="flex flex-wrap space-x-2 text-black font-bold">
+//           <li>Home</li>
+//           <li>&gt;</li>
+//           <li>My Account</li>
+//           <li>&gt;</li>
+//           <li>My Orders</li>
+//         </ol>
+//       </nav>
+//       <div className="flex flex-col gap-2 md:flex-row justify-between items-start my-4">
+//         <div className="flex flex-wrap w-full md:w-fit flex-col gap-2 md:flex-row">
+//           {/* Buttons for filtering orders */}
+//           <button
+//             onClick={() => handleButtonClick("All")}
+//             className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${activeButton === "All" ? "bg-rose-200" : "hover:bg-rose-300"
+//               }`}
+//           >
+//             All
+//           </button>
+//           <button
+//             onClick={() => handleButtonClick("In Progress")}
+//             className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${activeButton === "In Progress"
+//               ? "bg-rose-200"
+//               : "hover:bg-rose-300"
+//               }`}
+//           >
+//             In Progress
+//           </button>
+//           <button
+//             onClick={() => handleButtonClick("Delivered")}
+//             className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${activeButton === "Delivered" ? "bg-rose-200" : "hover:bg-rose-300"
+//               }`}
+//           >
+//             Delivered
+//           </button>
+//           <button
+//             onClick={() => handleButtonClick("Cancelled")}
+//             className={`py-2.5 px-5 text-sm font-medium text-black rounded-full border border-black ${activeButton === "Cancelled" ? "bg-rose-200" : "hover:bg-rose-300"
+//               }`}
+//           >
+//             Cancelled
+//           </button>
+//         </div>
+//         <div className="flex w-full md:w-fit items-center">
+//           <button className="py-2.5 px-5 w-full md:w-fit text-sm font-medium text-white bg-rose-400 rounded-full border border-black hover:bg-rose-300 flex items-center">
+//             <span className="mx-auto">Track Order</span>
+//           </button>
+//         </div>
+//       </div>
+//       <div className="space-y-4">
+//         {filteredOrders.map((order, index) => (
+//           <div key={index} className="bg-white shadow-md rounded-md p-8">
+//             <div className="flex items-center justify-between mb-4">
+//               <div className="flex items-center">
+
+//                 <div className="text-black font-medium text-xs bg-rose-200 rounded-full py-1.5 px-5">
+//                   {order.status}
+//                 </div>
+//                 {order.date && (
+//                   <div className="text-sm font-semibold text-black ml-4">
+//                     {order.date}
+//                   </div>
+//                 )}
+//               </div>
+//               <button
+//                 onClick={() => toggleTracking(index)}
+//                 className="mt-4 text-rose-400 hover:underline"
+//               >
+//                 {showTracking === index ? (
+//                   <MdKeyboardArrowDown className="inline" />
+//                 ) : (
+//                   <MdKeyboardArrowRight className="inline" />
+//                 )}
+//                 {showTracking === index ? " Hide Tracking" : " Show Tracking"}
+//               </button>
+//             </div>
+//             <div className="flex flex-col sm:flex-row items-center">
+//               <img
+//                 src={order.imageUrl}
+//                 alt="Product Image"
+//                 className="w-28 h-28 object-cover rounded-md mb-4 sm:mb-0 sm:mr-4"
+//               />
+//               <div className="flex-1">
+//                 <div className="text-gray-500 mb-2">
+//                   Order ID: {order.orderId}
+//                 </div>
+//                 <div className="text-black font-semibold text-xs mb-1">
+//                   {order.description}
+//                 </div>
+//                 <div
+//                   className={`text-${order.rating >= 4 ? "yellow" : "gray"
+//                     }-500 flex items-center`}
+//                 >
+//                   <div className="text-black text-xs font-semibold mr-1">
+//                     Rating:
+//                   </div>
+//                   {"★".repeat(order.rating)}
+//                   {"☆".repeat(5 - order.rating)}
+//                 </div>
+//                 <div className="font-semibold">{order.price}</div>
+//               </div>
+//             </div>
+//             {showTracking === index && (
+//               <DeliveryTracking orderStatus={order.status} />
+//             )}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
