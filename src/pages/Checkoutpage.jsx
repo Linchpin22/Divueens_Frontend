8f52e6de889d8629f58190fa02b93189d770f37a
deepanshu <gargdeepanshu601@gmail.com>
2024-08-03 20:05:47 +0530

    checkout payment changes

--- src/pages/Checkoutpage.jsx
+++ src/pages/Checkoutpage.jsx
@@ -1,125 +1,138 @@
+import {Link} from 'react-router-dom';
+import { MdDeleteOutline } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+
 export default function Checkoutpage(){
     return(
 
         <>
-        <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-  <a href="#" className="text-2xl font-bold text-gray-800">sneekpeeks</a>
-  <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
-    <div className="relative">
-      <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
-        <li className="flex items-center space-x-3 text-left sm:space-x-4">
-          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700" href="#"
-            ><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" /></svg
-          ></a>
-          <span className="font-semibold text-gray-900">Shop</span>
-        </li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-        </svg>
-        <li className="flex items-center space-x-3 text-left sm:space-x-4">
-          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">2</a>
-          <span className="font-semibold text-gray-900">Shipping</span>
-        </li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-        </svg>
-        <li className="flex items-center space-x-3 text-left sm:space-x-4">
-          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white" href="#">3</a>
-          <span className="font-semibold text-gray-500">Payment</span>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-<div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
-  <div className="px-4 pt-8">
-    <p className="text-xl font-medium">Order Summary</p>
-    <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
-    <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
-      <div className="flex flex-col rounded-lg bg-white sm:flex-row">
-        <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-        <div className="flex w-full flex-col px-4 py-4">
-          <span className="font-semibold">Nike Air Max Pro 8888 - Super Light</span>
-          <span className="float-right text-gray-400">42EU - 8.5US</span>
-          <p className="text-lg font-bold">$138.99</p>
-        </div>
-      </div>
-      
-    </div>
+       <div className="font-sans max-w-5xl max-md:max-w-xl mx-auto bg-white py-4">
+            <h1 className="text-3xl font-bold text-gray-800 text-center">Shopping Cart</h1>
 
-    
-  </div>
-  <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-    <p className="text-xl font-medium">Payment Details</p>
-    <p className="text-gray-400">Complete your order by providing your payment details.</p>
-    <div className="">
-      <label for="email" className="mt-4 mb-2 block text-sm font-medium">Email</label>
-      <div className="relative">
-        <input type="text" id="email" name="email" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="your.email@gmail.com" />
-        <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-          </svg>
-        </div>
-      </div>
-      <label for="card-holder" className="mt-4 mb-2 block text-sm font-medium">Card Holder</label>
-      <div className="relative">
-        <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
-        <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
-          </svg>
-        </div>
-      </div>
-      <label for="card-no" className="mt-4 mb-2 block text-sm font-medium">Card Details</label>
-      <div className="flex">
-        <div className="relative w-7/12 flex-shrink-0">
-          <input type="text" id="card-no" name="card-no" className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
-          <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-            <svg className="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
-              <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
-            </svg>
-          </div>
-        </div>
-        <input type="text" name="credit-expiry" className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="MM/YY" />
-        <input type="text" name="credit-cvc" className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="CVC" />
-      </div>
-      <label for="billing-address" className="mt-4 mb-2 block text-sm font-medium">Billing Address</label>
-      <div className="flex flex-col sm:flex-row">
-        <div className="relative flex-shrink-0 sm:w-7/12">
-          <input type="text" id="billing-address" name="billing-address" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Street Address" />
-          <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-            <img className="h-4 w-4 object-contain" src="https://flagpack.xyz/_nuxt/4c829b6c0131de7162790d2f897a90fd.svg" alt="" />
-          </div>
-        </div>
-        <select type="text" name="billing-state" className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500">
-          <option value="State">State</option>
-        </select>
-        <input type="text" name="billing-zip" className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="ZIP" />
-      </div>
+            <div className="grid md:grid-cols-3 gap-8 mt-16">
+                <div className="md:col-span-2 space-y-4">
 
-      
-      <div className="mt-6 border-t border-b py-2">
-        <div className="flex items-center justify-between">
-          <p className="text-sm font-medium text-gray-900">Subtotal</p>
-          <p className="font-semibold text-gray-900">$399.00</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <p className="text-sm font-medium text-gray-900">Shipping</p>
-          <p className="font-semibold text-gray-900">$8.00</p>
-        </div>
-      </div>
-      <div className="mt-6 flex items-center justify-between">
-        <p className="text-sm font-medium text-gray-900">Total</p>
-        <p className="text-2xl font-semibold text-gray-900">$408.00</p>
-      </div>
-    </div>
-    <button className="mt-4 mb-8 w-full rounded-md bg-rose-700 px-6 py-3 font-medium text-white">Place Order</button>
-  </div>
-</div>
 
+
+
+
+
+
+
+
+
+
+
+
+                    <div className="grid grid-cols-3 items-start gap-4">
+                        <div className="col-span-2 flex items-start gap-4">
+                            <div className="w-28 h-28 max-sm:w-24 max-sm:h-24 shrink-0 bg-gray-100 p-2 rounded-md">
+                                <img src='https://readymadeui.com/images/product14.webp' className="w-full h-full object-contain" />
+                            </div>
+
+                            <div className="flex flex-col">
+                                <h3 className="text-base font-bold text-gray-800">Velvet Sneaker</h3>
+                                <p className="text-xs font-semibold text-gray-500 mt-0.5">Size: MD</p>
+
+                                <button type="button" className="mt-6 font-semibold text-red-500 text-xs flex items-center gap-1 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 fill-current inline" viewBox="0 0 24 24">
+                                        <path d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z" data-original="#000000"></path>
+                                        <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z" data-original="#000000"></path>
+                                    </svg>
+                                    REMOVE
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="ml-auto">
+                            <h4 className="text-lg max-sm:text-base font-bold text-gray-800">$20.00</h4>
+
+                            <button type="button"
+                                className="mt-6 flex items-center px-3 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-2.5 fill-current" viewBox="0 0 124 124">
+                                    <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000"></path>
+                                </svg>
+
+                                <span className="mx-3 font-bold">2</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-2.5 fill-current" viewBox="0 0 42 42">
+                                    <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z" data-original="#000000"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                    <hr className="border-gray-300" />
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <div className="bg-gray-100 rounded-md p-4 h-max">
+                    <h3 className="text-lg max-sm:text-base font-bold text-gray-800 border-b border-gray-300 pb-2">Order Summary</h3>
+
+
+
+
+
+                  <div className=''>
+
+                    <div className='flex justify-between'>
+
+                    <h4 className="font-medium">address:</h4>
+                    <div className='flex mt-1.5 gap-x-2'><span className='text-lg'><MdDeleteOutline /></span>
+                    <span><FaEdit /></span></div>
+
+                    </div>
+
+                    <div><p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, sed?</p></div>
+
+                  </div>
+
+
+
+
+                  <div className='mt-6 text-rose-600 text-center border-2 border-rose-700'>
+                    <button><Link to="/Checkoutnewaddress" >Add New Address</Link></button>
+                  </div>
+
+                  
+
+
+
+
+
+
+
+                    <ul className="text-gray-800 mt-6 space-y-3">
+                        <li className="flex flex-wrap gap-4 text-sm">Subtotal <span className="ml-auto font-bold">$200.00</span></li>
+                        <li className="flex flex-wrap gap-4 text-sm">Shipping <span className="ml-auto font-bold">$2.00</span></li>
+                        <li className="flex flex-wrap gap-4 text-sm">Tax <span className="ml-auto font-bold">$4.00</span></li>
+                        <hr className="border-gray-300" />
+                        <li className="flex flex-wrap gap-4 text-sm font-bold">Total <span className="ml-auto">$206.00</span></li>
+                    </ul>
+
+                    <div className="mt-6 space-y-3">
+                        <button type="button" className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-rose-600 hover:bg-rose-700 text-white rounded-md"><Link to="/Paymentnow" >Payment Now</Link></button>
+                         <button type="button" className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md"> <Link to="/products">Continue Shopping</Link></button>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 };
