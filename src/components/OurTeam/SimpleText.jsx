a726769b27d638a5fd9e71bfbe29bb4d883b2f65
Linchpin22 <tiwarianuj712u@gmail.com>
2024-08-16 18:50:06 +0530

    Our Team slider change

--- src/components/OurTeam/SimpleText.jsx
+++ src/components/OurTeam/SimpleText.jsx
@@ -1,15 +1,87 @@
 // src/components/SimpleText.js
 import React from 'react';
-import './simpleText.css';
+// import './simpleText.css';
+import Card from './Card';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import '../input.css';
+import { FreeMode, Navigation } from 'swiper/modules';
+import influencer from '../../assets/AuthenticityPage/influencer.png'
+
+
+const testimonials = [
+  { image: influencer, name: 'Alexa', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+  { image: influencer, name: 'Diana', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+  { image: influencer, name: 'Hera', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+  { image: influencer, name: 'John', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+  { image: influencer, name: 'Jane', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+  { image: influencer, name: 'Mike', text: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit...', fullText: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.' },
+];
+
 
 const SimpleText = () => {
+
+  
   return (
-    <main className="container mx-auto text-center py-2">
-      <p className="text-5xl p-4 font-extrabold">
-        <span className="text-gradient-pink">Our Team</span> 
-      </p>
-    </main>
+    <div className='px-6 py-2'>
+
+    <div className="heading flex justify-center items-center">
+        <hr className="bg-gradient-to-l from-rose-700" data-aos="fade-right" />
+        <h2 className="text-xl text-nowrap text-center" data-aos="fade-up">Testimonials</h2>
+        <hr className="bg-gradient-to-r from-rose-700" data-aos="fade-left" />
+      </div>
+    
+    <div>
+    <div className="w-full relative flex flex-row items-center">
+      <button className="text-gray-600 absolute prevBtn border-[3px] border-black rounded-[50%] w-[30px] h-[30px] md:left-0 left-[-10px] z-10">
+        <FaArrowLeft size={20} className='mx-auto' />
+      </button>
+      <Swiper
+        breakpoints={{
+          200: {
+            slidesPerView: 1,
+            spaceBetween: 25,
+          },
+          376: {
+            slidesPerView: 2,
+            spaceBetween: 25,
+          },
+          800: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+          1240: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+          },
+        }}
+        modules={[FreeMode, Navigation]}
+        navigation={
+          {
+            prevEl: '.prevBtn',
+            nextEl: '.nextBtn',
+          }
+        }
+        freeMode={true}
+        className=' p-4'
+      >
+        {testimonials.map((data, index) => (
+          <SwiperSlide className='flex justify-center items-center' key={index}>
+            <Card image={data.image} text={data.text} fullText={data.fullText} name={data.name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button className="text-gray-600 absolute nextBtn border-[3px] border-black rounded-[50%] w-[30px] h-[30px] md:right-0 right-[-10px] z-10">
+        <FaArrowRight size={20} className='mx-auto'/>
+      </button>
+    </div>
+      </div>
+      </div>
   );
 };
 
-export default SimpleText;
+export default SimpleText;   
--- src/components/OurTeam/TeamMemberCard.jsx
+++ src/components/OurTeam/TeamMemberCard.jsx
@@ -6,21 +6,21 @@ const TeamMemberCard = ({ name, role, image, linkedin, twitter, speech, bgColor 
   const navigate = useNavigate();
   return (
     <div
-      className={`flex flex-col items-center justify-between p-4 w-full h-96 mx-auto  rounded-xl shadow-lg transform transition duration-500 hover:scale-105 ${bgColor}`}
+      className={`flex flex-col items-center justify-between p-4 w-full bg-rose-200 h-96 mx-auto rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:bg-rose-300`}
     >
       <div className="text-center space-y-2">
         <img className="h-32 w-32 rounded-full mx-auto" src={image} alt={`${name}'s profile`} />
         <div className="space-y-0.5">
-          <p className="text-xl font-bold mb-6">{name}</p>
-          <p className=" text-gray-900 ">{role}</p> 
-          <p className="text-gray-700">{speech}</p>
+          <p className="text-2xl font-bold mb-3">{name}</p>
+          <p className=" text-xl font-semibold">{role}</p> 
+          <p className="px-2 text-black">{speech}</p>
         </div>
       </div>
       <div className="flex space-x-4 mt-4"> 
-        <a onClick={()=> navigate("/linkedin")} target="_blank" rel="noopener noreferrer" className="text-rose-800 hover:text-rose-500">
+        <a onClick={()=> navigate("/linkedin")} target="_blank" rel="noopener noreferrer" className="text-black hover:text-zinc-700">
           <FaLinkedin className="text-2xl" />
         </a>
-        <a onClick={()=> navigate("/twitter")} target="_blank" rel="noopener noreferrer" className="text-rose-800 hover:text-rose-500">
+        <a onClick={()=> navigate("/twitter")} target="_blank" rel="noopener noreferrer" className="text-black hover:text-zinc-700">
           <FaTwitter className="text-2xl" />
         </a>
       </div>
